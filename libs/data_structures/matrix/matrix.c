e8acfb3a6b9386d5930f12a0d6f55d9dc056539d
Donpad <superlalko31@gmail.com>
2022-01-27 19:17:53 +0300

    libs connected

---
+++ libs/data_structures/matrix/matrix.c
@@ -0,0 +1,5 @@
+//
+// Created by Donpad on 27.01.2022.
+//
+
+#include "matrix.h"
---
+++ libs/data_structures/matrix/matrix.h
@@ -0,0 +1,8 @@
+//
+// Created by Donpad on 27.01.2022.
+//
+
+#ifndef LAB_5B_MATRIX_H
+#define LAB_5B_MATRIX_H
+
+#endif //LAB_5B_MATRIX_H
